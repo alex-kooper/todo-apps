6111b2b5c6c02978440517d1f88a57d0709ddc44
alex.kooper@gmail.com <alex.kooper@gmail.com>
2025-08-24 21:39:37 -0400

    Add function into TodoListService object

--- backends/scala/src/main/scala/app/domain/TodoListService.scala
+++ backends/scala/src/main/scala/app/domain/TodoListService.scala
@@ -21,3 +21,26 @@ trait TodoListService:
   ): IO[TodoListNotFoundError, TodoList]
 
   def deleteList(id: TodoListId): IO[TodoListNotFoundError, Unit]
+
+object TodoListService:
+  def lists: ZIO[TodoListService, Nothing, Seq[TodoList]] =
+    ZIO.serviceWithZIO(_.lists)
+
+  def listById(
+      id: TodoListId
+  ): ZIO[TodoListService, TodoListNotFoundError, TodoList] =
+    ZIO.serviceWithZIO(_.listById(id))
+
+  def newList(name: NonEmptyString): ZIO[TodoListService, Nothing, TodoList] =
+    ZIO.serviceWithZIO(_.newList(name))
+
+  def updateList(
+      id: TodoListId,
+      name: NonEmptyString
+  ): ZIO[TodoListService, TodoListNotFoundError, TodoList] =
+    ZIO.serviceWithZIO(_.updateList(id, name))
+
+  def deleteList(
+      id: TodoListId
+  ): ZIO[TodoListService, TodoListNotFoundError, Unit] =
+    ZIO.serviceWithZIO(_.deleteList(id))
